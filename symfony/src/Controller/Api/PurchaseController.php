adf3560fc1aa7c1c9da87c5a236729671fb663ca
petrozx <petroz@inbox.ru>
2023-08-20 18:46:00 +0000

    delete unused imports

--- symfony/src/Controller/Api/PurchaseController.php
+++ symfony/src/Controller/Api/PurchaseController.php
@@ -2,14 +2,10 @@
 
 namespace App\Controller\Api;
 
-
-use App\Repository\CouponRepository;
-use App\Repository\ProductRepository;
 use App\Service\PurchaseManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
